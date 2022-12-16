0881da0154b069c884ed98aecf9a260c2218ad9a
Adam Tovatt <sporedenstore@hotmail.com>
2022-12-16 14:01:34 +0100

    start work on tile and board

---
+++ Betapet/Models/InGame/Tile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Betapet.Models.InGame
+{
+    public enum TileType
+    {
+        Empty, MultiplyLetter, MultiplyWord, Letter
+    }
+
+    public class Tile
+    {
+        public TileType Type { get; set; }
+        public int NumericValue { get; set; }
+        public string StringValue { get; set; }
+
+        public Tile(TileType type, int numericValue)
+        {
+            Type = type;
+            NumericValue = numericValue;
+        }
+
+        public Tile(string letter)
+        {
+            Type = TileType.Letter;
+        }
+    }
+}
