293645724e6faa55fdd49a20f86896b754cd05fb
Adam Tovatt <sporedenstore@hotmail.com>
2023-01-05 22:21:12 +0100

    remove unused endpoints

--- betapet-bot-api/Controllers/BotController.cs
+++ betapet-bot-api/Controllers/BotController.cs
@@ -12,25 +12,6 @@ namespace BetapetBotApi.Controllers
     [Route("[controller]")]
     public class BotController : ControllerBase
     {
-        [HttpGet("handleMatches")]
-        public async Task<IActionResult> HandleMatches(string username, string password)
-        {
-            try
-            {
-                string connectionString = ConnectionStringHelper.GetConnectionStringFromUrl(EnvironmentHelper.GetEnvironmentVariable("DATABASE_URL"), SslMode.Prefer);
-                Bot bot = new Bot(username, password, "FF1912DED13658C431A222B5A7EA1D6DC6569E2C1A11E185FF81E7823C896B46", connectionString);
-
-                await bot.AcceptAllMatchRequests();
-
-                List<GameSummary> matchHandlingResponse = await bot.HandleAllMatches();
-                return new ApiResponse(matchHandlingResponse);
-            }
-            catch (ApiException exception)
-            {
-                return new ApiResponse(exception);
-            }
-        }
-
         [HttpPost("handleEverything")]
         public async Task<IActionResult> HandleEverything(string username, string password)
         {
@@ -72,37 +53,6 @@ namespace BetapetBotApi.Controllers
             {
                 return new ApiResponse(exception);
             }
-        }
-
-        [HttpGet("prediction")]
-        public async Task<IActionResult> GetPrediction()
-        {
-            string connectionString = ConnectionStringHelper.GetConnectionStringFromUrl(EnvironmentHelper.GetEnvironmentVariable("DATABASE_URL"), SslMode.Prefer);
-            Bot bot = new Bot("DavidRdrgz", "gunnaral", "FF1912DED13658C431A222B5A7EA1D6DC6569E2C1A11E185FF81E7823C896B46", connectionString);
-            return new ApiResponse(await bot.GetMessage());
-        }
-
-        [HttpGet("lexiconBenchMark")]
-        public async Task<IActionResult> GetBenchMark(int count, int length)
-        {
-            if(count == 0 || length == 0)
-                return new ApiResponse(new ApiException("Missing count and/or length in query parameter", System.Net.HttpStatusCode.BadRequest));
-
-            string connectionString = ConnectionStringHelper.GetConnectionStringFromUrl(EnvironmentHelper.GetEnvironmentVariable("DATABASE_URL"), SslMode.Prefer);
-            Bot bot = new Bot("DavidRdrgz", "gunnaral", "FF1912DED13658C431A222B5A7EA1D6DC6569E2C1A11E185FF81E7823C896B46", connectionString);
-
-            List<string> wordsOfLetters = new List<string>();
-
-            for (int i = 0; i < count; i++)
-            {
-                wordsOfLetters.Add(StringGenerator.GetRandomString(length));
-            }
-
-            Stopwatch stopwatch = Stopwatch.StartNew();
-            await bot.GetPossibleWords(wordsOfLetters);
-            stopwatch.Stop();
-
-            return new ApiResponse(new { totalTime = stopwatch.Elapsed.TotalMilliseconds, averageTime = (float)stopwatch.Elapsed.TotalMilliseconds / (float)count });
         }
     }
 }
